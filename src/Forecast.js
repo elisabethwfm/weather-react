13242766b3ce9420f04b5584ff4a697d689bd51e
elisabethwfm <elisabeth.wolfram@gmail.com>
2022-05-23 13:05:22 +0200

    prepared everthing to create a state so the information can be updated when searched

--- src/Forecast.js
+++ src/Forecast.js
@@ -7,36 +7,39 @@ export default function Forecast(props) {
     console.log(response.data);
   }
 
-  let apiKey = "bada8b7e78b2e8f21ed242b93f56b802";
-  let lat = props.coords.lat;
-  let lon = props.coords.lon;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  if (loaded) {
+    let apiKey = "bada8b7e78b2e8f21ed242b93f56b802";
+    let lat = props.coords.lat;
+    let lon = props.coords.lon;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="Forecast">
-      <div className="testGrid">
-        <div className="forecastGrid" id="forecastGrid">
-          <div className="days" id="days">
-            <p>Monday</p>
-          </div>
-          <div className="icons" id="icons">
-            <span className="cloudy" id="cloudy">
-              <WeatherIcon code="01d" className="icons" />
-            </span>
-          </div>
-          <div className="forecast" id="forecast">
-            <span className="maxTemp" id="max-temp">
-              18°C
-            </span>
-            °{" "}
-            <span className="minTemp" id="min-temp">
-              10°
-            </span>
+    axios.get(apiUrl).then(handleResponse);
+    return "Forecast loading...";
+  } else {
+    return (
+      <div className="Forecast">
+        <div className="testGrid">
+          <div className="forecastGrid" id="forecastGrid">
+            <div className="days" id="days">
+              <p>Monday</p>
+            </div>
+            <div className="icons" id="icons">
+              <span className="cloudy" id="cloudy">
+                <WeatherIcon code="01d" className="icons" />
+              </span>
+            </div>
+            <div className="forecast" id="forecast">
+              <span className="maxTemp" id="max-temp">
+                18°C
+              </span>
+              °{" "}
+              <span className="minTemp" id="min-temp">
+                10°
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
