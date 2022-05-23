1bf9d7deb91345ed10fb13bc00d8331872caf561
elisabethwfm <elisabeth.wolfram@gmail.com>
2022-05-23 12:47:27 +0200

    imported axios, and made preparations for the futre api call

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,7 +1,19 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcons";
+import axios from "axios";
 
-export default function Forecast() {
+export default function Forecast(response) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "bada8b7e78b2e8f21ed242b93f56b802";
+  let lat = 40.7;
+  let lon = 74;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="Forecast">
       <div className="testGrid">
