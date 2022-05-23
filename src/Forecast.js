0f351fa2790624af5fdd1c6954820293cf6b805b
elisabethwfm <elisabeth.wolfram@gmail.com>
2022-05-23 10:43:40 +0200

    included the first tempalte for the forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,5 +1,30 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcons";
 
 export default function Forecast() {
-  return <div className="Forecast">Forecast coming soon...</div>;
+  return (
+    <div className="Forecast">
+      <div className="testGrid">
+        <div className="forecastGrid" id="forecastGrid">
+          <div className="days" id="days">
+            <p>Monday</p>
+          </div>
+          <div className="icons" id="icons">
+            <span className="cloudy" id="cloudy">
+              <WeatherIcon code="01d" className="icons" />
+            </span>
+          </div>
+          <div className="forecast" id="forecast">
+            <span className="maxTemp" id="max-temp">
+              18°C
+            </span>
+            °
+            <span className="minTemp" id="min-temp">
+              Something°
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
