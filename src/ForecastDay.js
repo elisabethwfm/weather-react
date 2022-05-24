920aa40798ebc4f8065d4eeca9c808f0ea350dcc
elisabethwfm <elisabeth.wolfram@gmail.com>
2022-05-24 09:18:17 +0200

    Now the date for the forecast is receving the data from the api

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -2,22 +2,31 @@ import React from "react";
 import WeatherIcon from "./WeatherIcons";
 
 export default function ForecastDay(props) {
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
   return (
     <div className="ForecastDay">
       <div className="days" id="days">
-        <p>{props.data[0].dt}</p>
+        <p>{day()}</p>
       </div>
       <div className="icons" id="icons">
         <span className="cloudy" id="cloudy">
-          <WeatherIcon code={props.data[0].weather[0].icon} className="icons" />
+          <WeatherIcon code={props.data.weather[0].icon} className="icons" />
         </span>
       </div>
       <div className="forecast" id="forecast">
         <span className="maxTemp" id="max-temp">
-          {Math.round(props.data[0].temp.max)}°
+          {Math.round(props.data.temp.max)}°
         </span>{" "}
         <span className="minTemp" id="min-temp">
-          {Math.round(props.data[0].temp.min)}°
+          {Math.round(props.data.temp.min)}°
         </span>
       </div>
     </div>
